ab4b9c27c7457030d8288e32bcea7cf229fb4bdb
Thomas Siegmund <thomas.siegmund@googlemail.com>
2015-03-15 16:48:52 +0100

    add libs to ui.R

--- inst/examples/mixedsort/ui.R
+++ inst/examples/mixedsort/ui.R
@@ -2,6 +2,10 @@
 # Minimal shiny app demonstrating the D3TableFilter widget
 # ui.R
 # --------------------------------------------------------
+library(shiny)
+library(htmlwidgets)
+library(D3TableFilter)
+
 shinyUI(fluidPage(
   title = 'Basic usage of D3TableFilter in Shiny',
   fluidRow(
